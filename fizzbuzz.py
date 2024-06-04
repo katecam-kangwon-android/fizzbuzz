3b52e865bb27fff7d07bf8c3ed9f1cf84d8d9d90
jmc98 <jmc0820@naver.com>
2024-06-04 20:48:48 +0900

    Refactor(fizzbuzz.py): 수정 띄어쓰기

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,8 +3,8 @@ for i in range(1,15+1):
     if i% 15 ==0 :
     	print("fizzbuzz")
     elif i % 5 == 0:
-	print("buzz")
+	    print("buzz")
     elif i % 3 == 0:
-	print("fizz")
+	    print("fizz")
     else:
     	print(i)
