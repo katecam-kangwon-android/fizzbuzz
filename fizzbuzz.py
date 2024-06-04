2eac94ae168b6a0fc6aee74ff5ccd324d4c824d1
Joo Min Chul <56069969+joominchul@users.noreply.github.com>
2024-06-04 20:37:11 +0900

    Refactor: Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,10 @@
-# 1번 
+# 1번
 for i in range(1,15+1):
-  if i% 15 ==0 :
-    print("fizzbuzz")
-  elif i % 5 == 0:
-	  print("buzz")
-  elif i % 3 == 0:
-		print("fizz")
-  else:
-    print(i)
+    if i% 15 ==0 :
+    	print("fizzbuzz")
+    elif i % 5 == 0:
+	print("buzz")
+    elif i % 3 == 0:
+	print("fizz")
+    else:
+    	print(i)
